fe94eac59726e5cad8fa1cd280a249117f119e74
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-05-02 15:43:27 -0500

    Finished

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import random
 import json
+from smash import Character, Battle
 characters = []
 
 with open('characters.json') as json_file:
     characters = json.load(json_file)
+
+
+def find_character(name):
+    for character in characters:
+        if character["name"].lower() == name.lower():
+            return character
+    return None
+
+
+def play():
+    player_character = {}
+    player_choice = input("Enter your character's name: ")
+    if player_choice:
+        player_character = find_character(player_choice)
+    else:
+        player_character = random.choice(characters)
+    print("You choose {}".format(player_character["name"]))
+    comp_character = random.choice(characters)
+    print("Computer choose {}".format(comp_character['name']))
+    player = Character(player_character["name"], player_character["attacks"])
+    comp = Character(comp_character["name"], comp_character["attacks"])
+    print(player.name)
+    print(comp.name)
+
+    battle = Battle(player, comp)
+    battle.start()
+
+
+play()
--- smash.py
+++ smash.py
@@ -1,1 +1,36 @@
 import random
+
+
+class Character:
+    def __init__(self, name, moves):
+        self.name = name
+        self.moves = moves
+        self.health = 100
+
+    def attack(self):
+        return self.moves[random.randrange(len(self.moves))]
+
+    def get_hit(self, point):
+        self.health -= point
+
+
+class Battle:
+    def __init__(self, character1, character2):
+        self.character1 = character1
+        self.character2 = character2
+
+    def start(self):
+        while self.character1.health > 0 and self.character2.health > 0:
+            attack1 = self.character1.attack()
+            self.character2.get_hit(attack1["damage"])
+            print("{} is using {}, and damages {} {} health, remaining {} hp".format(
+                self.character1.name, attack1["name"], self.character2.name, attack1["damage"], self.character2.health))
+            attack2 = self.character1.attack()
+            self.character1.get_hit(attack2["damage"])
+            print("{} is using {}, and damages {} {} health,  remaining {} hp".format(
+                self.character2.name, attack2["name"], self.character2.name, attack1["damage"], self.character1.health))
+
+        if self.character1.health <= 0:
+            print("{} is the winner".format(self.character2.name))
+        else:
+            print("{} is the winner".format(self.character1.name))
